af25b322d64f8920faf0a44decb7c425e72f5c56
Lovish Bansal <lovish.bansal@harness.io>
2022-08-30 13:17:36 +0530

    Update main.tf

--- a1/main.tf
+++ a1/main.tf
@@ -1,12 +1,3 @@
-
-variable "instance_type" {
-  description = "instance type"
-}
-
-variable "ami" {
-  description = "AMI"
-}
-
 resource "aws_instance" "ArvindInstance" {
 	tags = {
 		Name = "Arvind_Terraform_Instance"
