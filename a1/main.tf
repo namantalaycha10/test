60d429c4fcd3c03308f33cf638cc41438fb75933
Lovish Bansal <lovish.bansal@harness.io>
2022-08-30 13:43:47 +0530

    Update main.tf

--- a1/main.tf
+++ a1/main.tf
@@ -1,11 +1,3 @@
-variable "instance_type" {
-  description = "instance type"
-}
-
-variable "ami" {
-  description = "AMI"
-}
-
 resource "aws_instance" "ArvindInstance" {
 	ami = "ami-0b69ea66ff7391e80"
 	instance_type = "t2.micro"
